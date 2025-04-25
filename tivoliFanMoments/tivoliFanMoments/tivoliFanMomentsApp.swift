9853c4f9baeb32745d9a9f30b67d5c3cd79cc099
Martin <ott>
2025-04-25 10:25:12 +0200

    Create empty project

---
+++ tivoliFanMoments/tivoliFanMoments/tivoliFanMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  tivoliFanMomentsApp.swift
+//  tivoliFanMoments
+//
+//  Created by Bofur on 25.04.25.
+//
+
+import SwiftUI
+
+@main
+struct tivoliFanMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
